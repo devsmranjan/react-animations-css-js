33343bd669b6765f77e724de9a2aec3c5f76e24c
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2020-07-24 00:12:24 +0530

    Updates

---
+++ precache-manifest.ba5d52cb436ea6d35660a30bdde03e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d837b9d06a3471774695ee61fbd2852a",
+    "url": "/react-animations-css-js/index.html"
+  },
+  {
+    "revision": "feb69c43193107c70815",
+    "url": "/react-animations-css-js/static/css/main.68de10c6.chunk.css"
+  },
+  {
+    "revision": "2d66832bbfa444c69631",
+    "url": "/react-animations-css-js/static/js/2.50dd6553.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-animations-css-js/static/js/2.50dd6553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb69c43193107c70815",
+    "url": "/react-animations-css-js/static/js/main.b62dadf9.chunk.js"
+  },
+  {
+    "revision": "15989232da6761f82bb4",
+    "url": "/react-animations-css-js/static/js/runtime-main.fe1bff6a.js"
+  }
+]);
